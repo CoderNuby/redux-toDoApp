0c51f7b513982b7136a33911a0db43454590b7b6
Luis Armando <asdf32easd@gmail.com>
2024-05-28 23:25:11 -0700

    template with angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { TodoModule } from './todos/todo.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
